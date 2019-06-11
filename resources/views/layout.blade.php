67bacc4172b1b82ed545570e9ba3c7bb767e0645
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-11 15:08:27 +0900

    Use layout template

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
