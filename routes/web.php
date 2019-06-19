91ad0a32777da96ed2c69990acc1f7a07358fa5a
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-19 18:25:50 +0900

    Add leading slash to router path

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('auth/twitter', 'Auth\TwitterAuthController@redirectToProvider');
-Route::get('auth/twitter/callback', 'Auth\TwitterAuthController@handleProviderCallback');
-Route::get('auth/twitter/logout', 'Auth\TwitterAuthController@logout');
+Route::get('/auth/twitter', 'Auth\TwitterAuthController@redirectToProvider');
+Route::get('/auth/twitter/callback', 'Auth\TwitterAuthController@handleProviderCallback');
+Route::get('/auth/twitter/logout', 'Auth\TwitterAuthController@logout');
