21af926624cb5c1788af148905edf7668f62beed
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-19 17:04:26 +0900

    Implement Twitter authentication

---
+++ app/Http/Auth/TwitterAuthController.php
@@ -0,0 +1,58 @@
+<?php
+namespace App\Http\Controllers\Auth;
+use App\User;
+use Auth;
+use Socialite;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class TwitterAuthController extends Controller
+{
+    use AuthenticatesUsers;
+
+    public function redirectToProvider()
+    {
+        return Socialite::driver('twitter')->redirect();
+    }
+
+    public function handleProviderCallback()
+    {
+        try {
+            $user = Socialite::driver('twitter')->user();
+        } catch (Exception $e) {
+            return redirect('auth/twitter');
+        }
+
+        $authUser = $this->findOrCreateUser($user);
+
+        Auth::login($authUser, true);
+
+        return redirect()->route("/players/{$authUser->nickname}");
+    }
+
+    private function findOrCreateUser($twitterUser)
+    {
+        $authUser = User::where('twitter_id', $twitterUser->id)->first();
+
+        if ($authUser) {
+            return $authUser;
+        }
+
+        return User::create([
+            // Use twitter screen name (@example without `@`) as name
+            'name' => strtolower($twitterUser->nickname),
+            'twitter_id' => $twitterUser->id,
+        ]);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route('login');
+    }
+
+    public function __construct()
+    {
+        $this->middleware('guest')->except('logout');
+    }
+}
