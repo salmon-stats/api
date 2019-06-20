5be67dc2ed3546198ae7fc2520efbb4b636097c2
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-20 15:45:46 +0900

    Update RedirectIfAuthenticated to redirect to own user page

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -18,7 +18,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            $user = Auth::user();
+            return redirect('users', ['id' => $user->id]);
         }
 
         return $next($request);
