57b2b836c07a282f3f58bc85a82132d49df5d710
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-21 12:50:51 +0900

    Add `player_id` column to `users`

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'name' => 'yukinkling2',
             'twitter_id' => 1090304898594308097,
+            'player_id' => '2cf69d9715b323b7',
             'api_token' => str_repeat('a', 64),
         ]);
     }
