13a14cfb478899662a0238da50614fed5e2a48b6
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-21 14:31:27 +0900

    Create table `salmon_weapons`

---
+++ database/migrations/2019_06_20_000005_create_salmon_weapons.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+use GuzzleHttp\Client;
+use function GuzzleHttp\json_decode;
+use Mockery\Exception\RuntimeException;
+
+class CreateSalmonWeapons extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $client = new Client();
+        $result = $client->get('https://stat.ink/api/v2/weapon');
+        if ($result->getStatusCode() == 200) {
+            $weapons = json_decode($result->getBody());
+            Log::debug($weapons);
+        } else {
+            throw new RuntimeException("Stat.ink API is unavailable.");
+        }
+
+        Schema::create('salmon_weapons', function (Blueprint $table) {
+            $table->unsignedSmallInteger('id');
+            $table->statInkKey('key', 32);
+
+            $table->primary('id');
+        });
+
+        // Filter weapon variants (e.g. Tentatek Splattershot to Splattershot)
+        array_filter($weapons, function ($weapon) {
+            return $weapon->{'main_ref'} === $weapon->{'key'};
+        });
+
+        array_push(
+            $weapons,
+            (object)['key' => 'kuma_blaster', 'splatnet' => 20000],
+            (object)['key' => 'kuma_brella',  'splatnet' => 20010],
+            (object)['key' => 'kuma_charger', 'splatnet' => 20020],
+            (object)['key' => 'kuma_slosher', 'splatnet' => 20030],
+        );
+
+        foreach ($weapons as $weapon) {
+            DB::table('salmon_weapons')->insert([
+                'id' => $weapon->{'splatnet'},
+                'key' => $weapon->{'key'},
+            ]);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('salmon_weapons');
+    }
+}
