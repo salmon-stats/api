e040fa146138620194806310c4a6361513ce57b2
yukidaruma <yukidaruma@users.noreply.github.com>
2019-06-22 14:04:41 +0900

    Use transaction in CreateSalmonWeapons

--- database/migrations/2019_06_20_000005_create_salmon_weapons.php
+++ database/migrations/2019_06_20_000005_create_salmon_weapons.php
@@ -17,40 +17,43 @@ class CreateSalmonWeapons extends Migration
      */
     public function up()
     {
-        $client = new Client();
-        $result = $client->get('https://stat.ink/api/v2/weapon');
-        if ($result->getStatusCode() == 200) {
-            $weapons = json_decode($result->getBody());
-        } else {
-            throw new RuntimeException("Stat.ink API is unavailable.");
-        }
+        DB::transaction(function () {
+            Schema::create('salmon_weapons', function (Blueprint $table) {
+                // Use negative integer for Grizzco weapons and random weapons
+                $table->unsignedSmallInteger('id');
+                $table->statInkKey('key', 32);
 
-        Schema::create('salmon_weapons', function (Blueprint $table) {
-            $table->unsignedSmallInteger('id');
-            $table->statInkKey('key', 32);
+                $table->primary('id');
+            });
 
-            $table->primary('id');
+            $client = new Client();
+            $result = $client->get('https://stat.ink/api/v2/weapon');
+            if ($result->getStatusCode() == 200) {
+                $weapons = json_decode($result->getBody());
+            } else {
+                throw new RuntimeException("Stat.ink API is unavailable.");
+            }
+
+            // Filter weapon variants (e.g. Tentatek Splattershot to Splattershot)
+            array_filter($weapons, function ($weapon) {
+                return $weapon->{'main_ref'} === $weapon->{'key'};
+            });
+
+            array_push(
+                $weapons,
+                (object)['key' => 'kuma_blaster', 'splatnet' => 20000],
+                (object)['key' => 'kuma_brella',  'splatnet' => 20010],
+                (object)['key' => 'kuma_charger', 'splatnet' => 20020],
+                (object)['key' => 'kuma_slosher', 'splatnet' => 20030],
+            );
+
+            foreach ($weapons as $weapon) {
+                DB::table('salmon_weapons')->insert([
+                    'id' => $weapon->{'splatnet'},
+                    'key' => $weapon->{'key'},
+                ]);
+            }
         });
-
-        // Filter weapon variants (e.g. Tentatek Splattershot to Splattershot)
-        array_filter($weapons, function ($weapon) {
-            return $weapon->{'main_ref'} === $weapon->{'key'};
-        });
-
-        array_push(
-            $weapons,
-            (object)['key' => 'kuma_blaster', 'splatnet' => 20000],
-            (object)['key' => 'kuma_brella',  'splatnet' => 20010],
-            (object)['key' => 'kuma_charger', 'splatnet' => 20020],
-            (object)['key' => 'kuma_slosher', 'splatnet' => 20030],
-        );
-
-        foreach ($weapons as $weapon) {
-            DB::table('salmon_weapons')->insert([
-                'id' => $weapon->{'splatnet'},
-                'key' => $weapon->{'key'},
-            ]);
-        }
     }
 
     /**
