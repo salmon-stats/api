34d58b900bf408ad9e2bec57cf610914716d61f7
yukidaruma <yukidaruma@users.noreply.github.com>
2019-08-23 04:13:15 +0900

    Use OatmealDome API to fetch Salmon Run schedules
    Because rare weapon data was not available on spla2.yuu26.com API

--- database/migrations/2019_06_20_000001_create_salmon_schedules_table.php
+++ database/migrations/2019_06_20_000001_create_salmon_schedules_table.php
@@ -29,40 +29,35 @@ class CreateSalmonSchedulesTable extends Migration
                 // $table->foreign('rare_weapon_id')->references('id')->on('salmon_weapons')->onDelete('cascade');
             });
 
-            $stageJapaneseNames = [
-                'シェケナダム' => 1,
-                '難破船ドン・ブラコ' => 2,
-                '海上集落シャケト場' => 3,
-                'トキシラズいぶし工房' => 4,
-                '朽ちた箱舟 ポラリス' => 5,
+            $stageIdtable = [
+                5000 => 1,
+                5001 => 2,
+                5002 => 3,
+                5003 => 4,
+                5004 => 5,
             ];
 
             $client = new Client();
-            $result = $client->get('https://spla2.yuu26.com/coop');
+
+            $result = $client->get('https://files.oatmealdome.me/bcat/coop.json');
             if ($result->getStatusCode() == 200) {
-                $schedules = json_decode($result->getBody())->result;
+                $schedules = json_decode($result->getBody())->Phases;
             } else {
-                throw new RuntimeException("spla2.yuu26.com API is unavailable.");
+                throw new RuntimeException('oatmealdome.me API is unavailable.');
             }
 
-            foreach ($schedules as $schedule) {
-                if (!$schedule->weapons) {
-                    continue;
-                }
+            foreach ($schedules as $rawSchedule) {
+                $startTime = Carbon::parse($rawSchedule->StartDateTime);
+                $endTime = Carbon::parse($rawSchedule->EndDateTime);
 
-                $weapons = array_map(function ($weapon) {
-                    return $weapon->id;
-                }, $schedule->weapons);
-                $startTime = Carbon::parse($schedule->start_utc);
-                $endTime = Carbon::parse($schedule->end_utc);
-
-                DB::table('salmon_schedules')->insert([
+                $schedule = new \App\SalmonSchedule([
                     'schedule_id' => $startTime,
                     'end_at' => $endTime,
-                    'weapons' => json_encode($weapons),
-                    'rare_weapon_id' => 20000,
-                    'stage_id' => $stageJapaneseNames[$schedule->stage->name],
+                    'weapons' => json_encode($rawSchedule->WeaponSets),
+                    'rare_weapon_id' => $rawSchedule->RareWeaponID,
+                    'stage_id' => $stageIdtable[$rawSchedule->StageID],
                 ]);
+                $schedule->save();
             }
         });
     }
