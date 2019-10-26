620a4cb1388e49ae730ff90a5b4a44f57e5b04c3
yukidaruma <yukidaruma@users.noreply.github.com>
2019-10-27 01:12:27 +0900

    Make global stats available when player has no results during the schedule

--- app/Http/Controllers/SalmonPlayerScheduleController.php
+++ app/Http/Controllers/SalmonPlayerScheduleController.php
@@ -10,6 +10,98 @@ use Illuminate\Support\Facades\DB;
 
 class SalmonPlayerScheduleController extends Controller
 {
+    private function getPlayerSummary(Request $request)
+    {
+        $scheduleId = Helper::scheduleIdToTimestamp($request->schedule_id);
+
+        $summaryQuery = <<<QUERY
+        COUNT(*) AS games,
+        CONVERT(SUM(CASE WHEN fail_reason_id IS NULL THEN 1 ELSE 0 END), UNSIGNED) AS clear_games,
+        CONVERT(SUM(clear_waves), UNSIGNED) AS clear_waves,
+        CONVERT(SUM(boss_appearance_count), UNSIGNED) AS boss_appearance_count,
+        CONVERT(SUM(boss_elimination_count), UNSIGNED) AS boss_elimination_count,
+        SUM(JSON_EXTRACT(boss_appearances, '$."3"')) AS 'boss_appearance_3',
+        SUM(JSON_EXTRACT(boss_appearances, '$."6"')) AS 'boss_appearance_6',
+        SUM(JSON_EXTRACT(boss_appearances, '$."9"')) AS 'boss_appearance_9',
+        SUM(JSON_EXTRACT(boss_appearances, '$."12"')) AS 'boss_appearance_12',
+        SUM(JSON_EXTRACT(boss_appearances, '$."13"')) AS 'boss_appearance_13',
+        SUM(JSON_EXTRACT(boss_appearances, '$."14"')) AS 'boss_appearance_14',
+        SUM(JSON_EXTRACT(boss_appearances, '$."15"')) AS 'boss_appearance_15',
+        SUM(JSON_EXTRACT(boss_appearances, '$."16"')) AS 'boss_appearance_16',
+        SUM(JSON_EXTRACT(boss_appearances, '$."21"')) AS 'boss_appearance_21'
+        QUERY;
+
+        $summary = SalmonResult::where('schedule_id', $scheduleId)
+            ->whereJsonContains('members', $request->player_id)
+            ->select(DB::raw($summaryQuery))
+            ->where('salmon_results.golden_egg_delivered', '!=', 0)
+            ->where('salmon_results.power_egg_collected', '!=', 0)
+            ->first()
+            ->toArray();
+
+        $scoresQuery = <<<QUERY
+        SELECT
+            CASE WHEN salmon_player_results.player_id = ? THEN true ELSE false END AS is_myself,
+            CONVERT(SUM(rescue), UNSIGNED) AS rescue,
+            CONVERT(SUM(death), UNSIGNED) AS death,
+            CONVERT(SUM(salmon_player_results.boss_elimination_count), UNSIGNED) AS boss_elimination_count,
+            CONVERT(SUM(golden_eggs), UNSIGNED) AS golden_eggs,
+            CONVERT(SUM(power_eggs), UNSIGNED) AS power_eggs,
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."3"')) AS 'boss_elimination_3',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."6"')) AS 'boss_elimination_6',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."9"')) AS 'boss_elimination_9',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."12"')) AS 'boss_elimination_12',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."13"')) AS 'boss_elimination_13',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."14"')) AS 'boss_elimination_14',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."15"')) AS 'boss_elimination_15',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."16"')) AS 'boss_elimination_16',
+            SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."21"')) AS 'boss_elimination_21'
+
+            FROM salmon_results
+            LEFT OUTER JOIN salmon_player_results ON
+                salmon_player_results.salmon_id = salmon_results.id
+            INNER JOIN salmon_player_boss_eliminations ON
+                salmon_player_boss_eliminations.salmon_id = salmon_results.id AND
+                salmon_player_boss_eliminations.player_id = salmon_player_results.player_id
+            WHERE
+                schedule_id = ? AND
+                json_contains(members, ?) AND
+                salmon_results.golden_egg_delivered != 0 AND
+                salmon_results.power_egg_collected != 0
+            GROUP BY is_myself
+            ORDER BY is_myself ASC
+        QUERY;
+
+        $scores = collect(DB::select($scoresQuery, [
+            $request->player_id,
+            $scheduleId,
+            "[\"$request->player_id\"]",
+        ]))
+            ->map(function ($_scores) {
+                $isMyself = $_scores->is_myself === 1;
+                $scores = get_object_vars($_scores);
+
+                foreach ($scores as $key => $value) {
+                    if ($key === 'is_myself') {
+                        unset($scores[$key]);
+                    }
+                    else {
+                        $newKey = ($isMyself ? 'player_' : 'others_') . $key;
+                        $scores[$newKey] = $value;
+                        unset($scores[$key]);
+                    }
+                }
+                return $scores;
+            })
+            ->collapse()
+            ->toArray();
+
+        return array_merge(
+            $summary,
+            $scores,
+        );
+    }
+
     function index(Request $request)
     {
         return SalmonPlayerResult::select(DB::raw(<<<QUERY
@@ -58,10 +150,6 @@ QUERY;
             'json_player_id' => "[\"$request->player_id\"]",
             'schedule_id' => $scheduleId,
         ]);
-
-        if (count($weapons) === 0) {
-            abort(404);
-        }
 
         $globalTeamSummaryQuery = <<<QUERY
 COUNT(*) AS games,
@@ -116,101 +204,25 @@ QUERY;
             $scheduleId,
         ]);
 
-        $summaryQuery = <<<QUERY
-COUNT(*) AS games,
-CONVERT(SUM(CASE WHEN fail_reason_id IS NULL THEN 1 ELSE 0 END), UNSIGNED) AS clear_games,
-CONVERT(SUM(clear_waves), UNSIGNED) AS clear_waves,
-CONVERT(SUM(boss_appearance_count), UNSIGNED) AS boss_appearance_count,
-CONVERT(SUM(boss_elimination_count), UNSIGNED) AS boss_elimination_count,
-SUM(JSON_EXTRACT(boss_appearances, '$."3"')) AS 'boss_appearance_3',
-SUM(JSON_EXTRACT(boss_appearances, '$."6"')) AS 'boss_appearance_6',
-SUM(JSON_EXTRACT(boss_appearances, '$."9"')) AS 'boss_appearance_9',
-SUM(JSON_EXTRACT(boss_appearances, '$."12"')) AS 'boss_appearance_12',
-SUM(JSON_EXTRACT(boss_appearances, '$."13"')) AS 'boss_appearance_13',
-SUM(JSON_EXTRACT(boss_appearances, '$."14"')) AS 'boss_appearance_14',
-SUM(JSON_EXTRACT(boss_appearances, '$."15"')) AS 'boss_appearance_15',
-SUM(JSON_EXTRACT(boss_appearances, '$."16"')) AS 'boss_appearance_16',
-SUM(JSON_EXTRACT(boss_appearances, '$."21"')) AS 'boss_appearance_21'
-QUERY;
-
-        $summary = SalmonResult::where('schedule_id', $scheduleId)
-            ->whereJsonContains('members', $request->player_id)
-            ->select(DB::raw($summaryQuery))
-            ->where('salmon_results.golden_egg_delivered', '!=', 0)
-            ->where('salmon_results.power_egg_collected', '!=', 0)
-            ->first()
-            ->toArray();
-
-        $scoresQuery = <<<QUERY
-SELECT
-    CASE WHEN salmon_player_results.player_id = ? THEN true ELSE false END AS is_myself,
-    CONVERT(SUM(rescue), UNSIGNED) AS rescue,
-    CONVERT(SUM(death), UNSIGNED) AS death,
-    CONVERT(SUM(salmon_player_results.boss_elimination_count), UNSIGNED) AS boss_elimination_count,
-    CONVERT(SUM(golden_eggs), UNSIGNED) AS golden_eggs,
-    CONVERT(SUM(power_eggs), UNSIGNED) AS power_eggs,
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."3"')) AS 'boss_elimination_3',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."6"')) AS 'boss_elimination_6',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."9"')) AS 'boss_elimination_9',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."12"')) AS 'boss_elimination_12',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."13"')) AS 'boss_elimination_13',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."14"')) AS 'boss_elimination_14',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."15"')) AS 'boss_elimination_15',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."16"')) AS 'boss_elimination_16',
-    SUM(JSON_EXTRACT(salmon_player_boss_eliminations.counts, '$."21"')) AS 'boss_elimination_21'
-
-    FROM salmon_results
-    LEFT OUTER JOIN salmon_player_results ON
-        salmon_player_results.salmon_id = salmon_results.id
-    INNER JOIN salmon_player_boss_eliminations ON
-        salmon_player_boss_eliminations.salmon_id = salmon_results.id AND
-        salmon_player_boss_eliminations.player_id = salmon_player_results.player_id
-    WHERE
-        schedule_id = ? AND
-        json_contains(members, ?) AND
-        salmon_results.golden_egg_delivered != 0 AND
-        salmon_results.power_egg_collected != 0
-    GROUP BY is_myself
-    ORDER BY is_myself ASC
-QUERY;
-
-        $scores = collect(DB::select($scoresQuery, [
-            $request->player_id,
-            $scheduleId,
-            "[\"$request->player_id\"]",
-        ]))
-            ->map(function ($_scores) {
-                $isMyself = $_scores->is_myself === 1;
-                $scores = get_object_vars($_scores);
-
-                foreach ($scores as $key => $value) {
-                    if ($key === 'is_myself') {
-                        unset($scores[$key]);
-                    }
-                    else {
-                        $newKey = ($isMyself ? 'player_' : 'others_') . $key;
-                        $scores[$newKey] = $value;
-                        unset($scores[$key]);
-                    }
-                }
-                return $scores;
-            })
-            ->collapse()
-            ->toArray();
-
-        $results = app()->call('App\Http\Controllers\SalmonResultController@index');
-
-        return [
+        $response = [
             'global' => array_merge(
                 $globalTeamSummary[0]->toArray(),
                 get_object_vars($globalPlayerSummary[0]),
             ),
-            'summary' => array_merge(
-                $summary,
-                $scores,
-            ),
+        ];
+
+        if (count($weapons) === 0) {
+            return $response;
+        }
+
+        $results = app()->call('App\Http\Controllers\SalmonResultController@index');
+
+        $personal_stats = [
+            'summary' => $this->getPlayerSummary($request),
             'weapons' => $weapons,
             'results' => $results->items(),
         ];
-   }
+
+        return array_merge($response, $personal_stats);
+    }
 }
