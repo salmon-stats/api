d49abcd8b7f286589b4b29b5e8761525ba41bd0b
yukidaruma <yukidaruma@users.noreply.github.com>
2019-11-07 11:25:24 +0900

    Use `display_name` and `show_twitter_avatar` settings for `User` model

--- app/User.php
+++ app/User.php
@@ -10,14 +10,18 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    protected $casts = [
+        'show_twitter_avatar' => 'boolean',
+    ];
+
     protected $rememberTokenName = false;
 
     protected $fillable = [
-        'name', 'twitter_id', 'api_token', 'twitter_avatar'
+        'name', 'twitter_id', 'api_token', 'twitter_avatar', 'display_name', 'show_twitter_avatar',
     ];
 
     protected $hidden = [
-        'api_token', 'created_at', 'twitter_id',
+        'api_token', 'created_at', 'twitter_id', 'display_name', 'show_twitter_avatar',
     ];
 
     /**
@@ -27,6 +31,24 @@ class User extends Authenticatable
     public function getPlayerPage()
     {
         if ($this->player_id) return env('APP_FRONTEND_ORIGIN') . "/players/{$this->player_id}";
+        return null;
+    }
+
+    public function getNameAttribute()
+    {
+        if ($this->display_name) {
+            return $this->display_name;
+        }
+
+        return $this->name;
+    }
+
+    public function getTwitterAvatarAttribute()
+    {
+        if ($this->show_twitter_avatar) {
+            return $this->getOriginal('twitter_avatar');
+        }
+
         return null;
     }
 
