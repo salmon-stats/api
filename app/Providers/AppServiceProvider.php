bcb1ea215a59fd0626e0794650ce7a66bdd4e22b
yukidaruma <yukidaruma@users.noreply.github.com>
2020-06-28 14:21:45 +0900

    Make sure to use `https` in production environment
    When running behind reverse proxy, it redirected to http url instead of https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\UrlGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,8 +22,10 @@ class AppServiceProvider extends ServiceProvider
      *
      * @return void
      */
-    public function boot()
+    public function boot(UrlGenerator $url)
     {
-        //
+        if (\App::environment() === 'production') {
+          $url->forceScheme('https');
+        }
     }
 }
