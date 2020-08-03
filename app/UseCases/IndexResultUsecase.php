31e92ce962f45a409df1a674658c1beb67a5d16b
yukidaruma <yukidaruma@users.noreply.github.com>
2020-08-03 22:24:55 +0900

    Extract `queryFullResults` as `SalmonResultQueryHelper` from `IndexResultUseCase`

--- app/UseCases/IndexResultUsecase.php
+++ app/UseCases/IndexResultUsecase.php
@@ -2,13 +2,10 @@
 
 namespace App\UseCases;
 
+use App\Helpers\SalmonResultQueryHelper;
 use App\Helpers\SalmonResultsFilterHelper;
-use App\SalmonPlayerBossElimination;
 use App\SalmonResult;
 use App\SalmonPlayerResult;
-use App\SalmonPlayerSpecialUse;
-use App\SalmonPlayerWeapon;
-use App\SalmonWave;
 
 class IndexResultUsecase
 {
@@ -84,74 +81,9 @@ class IndexResultUsecase
         $results = $resultsWithPagination['data'];
         unset($resultsWithPagination['data']);
 
-        $ids = array_map(
-            fn ($result) => $result['id'],
-            $results,
-        );
-
-        $playerBossEliminations = SalmonPlayerBossElimination::whereIn('salmon_id', $ids)->get()->makeVisible(['salmon_id', 'player_id'])->toArray();
-        $playerResults = SalmonPlayerResult::whereIn('salmon_id', $ids)->get()->makeVisible('salmon_id')->toArray();
-        $playerWeapons = SalmonPlayerWeapon::whereIn('salmon_id', $ids)->get()->makeVisible(['salmon_id', 'player_id'])->toArray();
-        $waveResults = SalmonWave::whereIn('salmon_id', $ids)->get()->makeVisible('salmon_id')->toArray();
-        $specialUses = SalmonPlayerSpecialUse::whereIn('salmon_id', $ids)->get()->makeVisible(['salmon_id', 'player_id'])->toArray();
-
-        $results = array_map(
-            function ($salmonResult) use ($playerBossEliminations, $playerResults, $playerWeapons, $waveResults, $specialUses) {
-                $playerResult = $this->filterRelevantResults($salmonResult['id'], $playerResults);
-                $waveResult = $this->filterRelevantResults($salmonResult['id'], $waveResults);
-
-                foreach ($playerResult as &$player) {
-                    $player = array_merge(
-                        $player,
-                        [
-                            'boss_eliminations' => $this->filterRelevantResults($salmonResult['id'], $playerBossEliminations, $player['player_id'])[0],
-                            'weapons' => $this->filterRelevantResults($salmonResult['id'], $playerWeapons, $player['player_id']),
-                            'special_uses' => $this->filterRelevantResults($salmonResult['id'], $specialUses, $player['player_id']),
-                        ],
-                    );
-                }
-
-                return array_merge(
-                    $salmonResult,
-                    [
-                        'player_results' => $playerResult,
-                        'waves' => $waveResult,
-                    ],
-                );
-            },
-            $results,
-        );
-
         return array_merge(
             $resultsWithPagination,
-            ['results' => $results],
-        );
-    }
-
-    private function filterRelevantResults(int $salmonId, array $array, ?string $playerId = null) {
-        $hasPlayerId = $playerId !== null;
-
-        return $this->valuesWithoutSalmonId(
-            array_filter(
-                $array,
-                fn ($result) => $result['salmon_id'] === $salmonId &&
-                    (!$hasPlayerId || $result['player_id'] === $playerId),
-            ),
-            $hasPlayerId,
-        );
-    }
-
-    private function valuesWithoutSalmonId(array $array, bool $hasPlayerId) {
-        return array_map(
-            function ($value) use ($hasPlayerId) {
-                unset($value['salmon_id']);
-                if ($hasPlayerId) {
-                    unset($value['player_id']);
-                }
-
-                return $value;
-            },
-            array_values($array),
+            ['results' => SalmonResultQueryHelper::queryFullResults($results)],
         );
     }
 }
